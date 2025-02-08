6c146bb74e15c26abd0a6d3c14c44b958f2bea00
hjk502 <2559607462@qq.com>
2025-02-08 19:59:12 +0800

    Character Blueprint Setup

--- Aura/Private/Character/AuraCharacterBase.cpp
+++ Aura/Private/Character/AuraCharacterBase.cpp
@@ -8,6 +8,11 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon=CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(),FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
 	
 }
 
