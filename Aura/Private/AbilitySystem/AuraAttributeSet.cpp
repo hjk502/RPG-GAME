ea7fef51c865e82eccc79b43666a494bef71c6f1
hjk502 <2559607462@qq.com>
2025-02-10 18:49:49 +0800

    Health and Mana

--- Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,4 +2,50 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UAuraAttributeSet::UAuraAttributeSet()
+{
+	
+}
+
+/////////////////////////////////////////////////////////////////
+/// AuraAttributeSet replicate system 
+/////////////////////////////////////////////////////////////////
+
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	//REPNOTIFY_Always means health will replicate every condition
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,Health,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,MaxHealth,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,Mana,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,MaxMana,COND_None,REPNOTIFY_Always);
+}
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	//notify the AbilitySystemComponent the health has change
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Health,OldHealth);
+}
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	//notify the AbilitySystemComponent the max health has change
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,MaxHealth,OldMaxHealth);
+}
+
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	//notify the AbilitySystemComponent the max health has change
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Mana,OldMana);
+}
+
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	//notify the AbilitySystemComponent the maxMana has change
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,MaxMana,OldMaxMana);
+}
 
--- Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,5 +13,46 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+private:
 	
+protected:
+
+public:
+	UAuraAttributeSet();
+
+	//the function to register replicate variable
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	//character/Enemy's current health
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Health,Category="Vital Attributes")
+	FGameplayAttributeData Health;
+
+	//character/Enemy's MaxHealth
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_MaxHealth,Category="Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	//character/Enemy's current mana
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Mana,Category="Vital Attributes")
+	FGameplayAttributeData Mana;
+
+	//character/Enemy's MaxMana
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_MaxMana,Category="Vital Attributes")
+	FGameplayAttributeData MaxMana;
+
+	//the function call on the client when Health change on server
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	//the function call on the client when MaxHealth change on server
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	//the function call on the client when Mana change on server
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	//the function call on the client when MaxMana change on server
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
