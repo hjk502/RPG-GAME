b7bc8eac9e6d3092e586d8e4c02e4325af567a7f
hjk502 <2559607462@qq.com>
2025-02-12 15:49:46 +0800

    Callbacks for Mana Changes

--- Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -8,7 +8,8 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature,float,NewHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature,float,NewMaxHealth);
-
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature,float,NewMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature,float,NewMaxMana);
 
 /**
  * 
@@ -21,11 +22,17 @@ private:
 	
 protected:
 
-	//when abilitySystemComponent influence the Health,call widget controller to change it on HUD
+	//when Health change,call widget controller to change it on HUD
 	void HealthChanged(const FOnAttributeChangeData& Data) const;
 
-	//when abilitySystemComponent influence the MaxHealth,call widget controller to change it on HUD
+	//when MaxHealth change,call widget controller to change it on HUD
 	void MaxHealthChanged(const FOnAttributeChangeData& Data)const;
+
+	//when Mana change,call widget controller to change it on HUD
+	void ManaChanged(const FOnAttributeChangeData& Data)const;
+
+	//when MaxMana change,call widget controller to change it on HUD
+	void MaxManaChanged(const FOnAttributeChangeData& Data)const;
 	
 public:
 
@@ -42,4 +49,12 @@ public:
 	//bind the function call when the MaxHealth change,Overlay will listen it and change variable on HUD
 	UPROPERTY(BlueprintAssignable,Category="GAS|Attributes")
 	FOnMaxHealthChangedSignature OnMaxHealthChanged;
+
+	//bind the function call when the mana change,Overlay will listen it and change variable on HUD
+	UPROPERTY(BlueprintAssignable,Category="GAS|Attributes")
+	FOnManaChangedSignature OnManaChanged;
+
+	//bind the function call when the MaxMana change,Overlay will listen it and change variable on HUD
+	UPROPERTY(BlueprintAssignable,Category="GAS|Attributes")
+	FOnMaxManaChangedSignature OnMaxManaChanged;
 };
