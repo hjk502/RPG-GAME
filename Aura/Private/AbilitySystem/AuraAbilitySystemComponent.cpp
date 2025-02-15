32bc4662a5821150e97e9b231840f996f2563099
hjk502 <2559607462@qq.com>
2025-02-15 15:24:12 +0800

    Get All Asset Tags

--- Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -11,6 +11,15 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1,8.f,FColor::Blue,FString("Effect Applied!"));
+
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for(const FGameplayTag& Tag:TagContainer)
+	{
+		//TODO: BoardCast the tag to widget controller
+		const FString Msg=FString::Printf(TEXT("GE Tag: %s"),*Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue,Msg);
+		
+	}
 	
 }
